5149cd0029fe73d3e1669f3795a315f9a91fa759
mystic-cg <215104920@qq.com>
2019-10-13 17:29:40 +0800

    [ongoing]

--- app/src/main/java/individual/cc/app/configuration/RestApplication.java
+++ app/src/main/java/individual/cc/app/configuration/RestApplication.java
@@ -1,12 +1,26 @@
 package individual.cc.app.configuration;
 
+import individual.cc.jar.bean.session.SimpleStatelessEjb;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ejb.EJB;
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/rest")
 public class RestApplication extends ResourceConfig {
+
+    @EJB
+    private SimpleStatelessEjb simpleStatelessEjb;
+
     public RestApplication() {
-        packages("individual.cc.app.servlet.HelloController");
+        packages("individual.cc.app.servlet");
+
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(simpleStatelessEjb).to(SimpleStatelessEjb.class);
+            }
+        });
     }
 }

dbd8371148db6404dba53bfdc9b20620d927d850
mystic-cg <215104920@qq.com>
2019-10-21 20:58:31 +0800

    [ongoing]

--- app/src/main/java/individual/cc/app/servlet/HelloController.java
+++ app/src/main/java/individual/cc/app/servlet/HelloController.java
@@ -3,12 +3,14 @@ package individual.cc.app.servlet;
 import individual.cc.jar.bean.session.SimpleStatelessEjb;
 
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
+@Stateless
 @Path("hello")
 public class HelloController {
 
